0b3133bc5dd3c53566b043a342b14f1bac1df960
Rinde van Lon <rindevanlon@gmail.com>
2016-03-18 17:01:12 +0100

    updated experiment

--- sensitivity-experiment.sh
+++ sensitivity-experiment.sh
@@ -1,2 +1,2 @@
 export MAVEN_OPTS=-server
-nohup mvn exec:java -Dexec.mainClass="com.github.rinde.jaamas16.PerformExperiment" -Dexec.args="-exp time_deviation -g false -i c8,c55 -t 1 -r 1 -w 30000 -o SEED_REPS,REPS,SCENARIO,CONFIG" &
+nohup mvn exec:java -Dexec.mainClass="com.github.rinde.jaamas16.PerformExperiment" -Dexec.args="-exp time_deviation -g false -i c55 -t 11 -r 1 -w 30000 -o SEED_REPS,REPS,SCENARIO,CONFIG" &
