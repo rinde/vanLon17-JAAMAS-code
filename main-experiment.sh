fdaf1a4cdf449afee16c68d3b7717caa6cd2db73
Rinde van Lon <rindevanlon@gmail.com>
2016-03-29 15:02:24 +0200

    updated main experiment script

--- main-experiment.sh
+++ main-experiment.sh
@@ -1,2 +1,2 @@
 export MAVEN_OPTS=-server
-nohup mvn exec:java -Dexec.mainClass="com.github.rinde.jaamas16.PerformExperiment" -Dexec.args="-exp vanlon15 -g false -i c8,c55 -t 11 -r 3 -sf glob:**[0-9].scen -w 30000 -o SEED_REPS,REPS,SCENARIO,CONFIG" &
+nohup mvn exec:java -Dexec.mainClass="com.github.rinde.jaamas16.PerformExperiment" -Dexec.args="-exp vanlon15 -g false -i c8,c43 -t 11 -r 3 -sf glob:**[0-9].scen -w 30000 -o SEED_REPS,REPS,SCENARIO,CONFIG" &
