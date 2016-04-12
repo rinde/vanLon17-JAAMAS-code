0994996444603193eb90a97fe8dffff5c1d99282
Rinde van Lon <rindevanlon@gmail.com>
2016-04-12 15:41:51 +0200

    updated experiment script

--- main-experiment-mas90.sh
+++
@@ -1,2 +0,0 @@
-export MAVEN_OPTS=-server
-nohup mvn exec:java -Dexec.mainClass="com.github.rinde.jaamas16.PerformExperiment" -Dexec.args="-exp vanlon15 -g false -i c14 -t 11 -r 3 -sf glob:**0.20-**-10.00-[0-9].scen -w 30000 -o SEED_REPS,REPS,SCENARIO,CONFIG" &
