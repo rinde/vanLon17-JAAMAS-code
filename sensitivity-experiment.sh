1876251d4d90d1984a5d81b0331e326e1e129df2
Rinde van Lon <rindevanlon@gmail.com>
2016-06-03 16:38:05 +0200

    new sensitity experiment settings

--- sensitivity-experiment.sh
+++ sensitivity-experiment.sh
@@ -1,2 +1,2 @@
 export MAVEN_OPTS=-server
-nohup mvn exec:java -Dexec.mainClass="com.github.rinde.jaamas16.PerformExperiment" -Dexec.args="-exp time_deviation -g false -i c43 -t 11 -r 1 -w 30000 -o SEED_REPS,REPS,SCENARIO,CONFIG" &
+nohup mvn exec:java -Dexec.mainClass="com.github.rinde.jaamas16.PerformExperiment" -Dexec.args="-exp time_deviation -g false -i c14,c55 -t 11 -r 1 -seed-repetitions 100 -w 30000 -sf glob:**0.50-20-10.00-0.scen -o SEED_REPS,REPS,SCENARIO,CONFIG" &
