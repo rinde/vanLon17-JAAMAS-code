cddb2cb9dd048f02f4a9d509f826ae75e1797f21
Rinde van Lon <rindevanlon@gmail.com>
2016-06-06 11:12:37 +0200

    additional sensitivity experiment

---
+++ sensitivity-experiment-part2.sh
@@ -0,0 +1,2 @@
+export MAVEN_OPTS=-server
+nohup mvn exec:java -Dexec.mainClass="com.github.rinde.jaamas16.PerformExperiment" -Dexec.args="-exp time_deviation -g false -i c38,c39 -t 11 -r 1 -seed-repetitions 100 -w 30000 -sf glob:**0.50-20-10.00-0.scen -o SEED_REPS,REPS,SCENARIO,CONFIG" &
