0d36908297a0c14661137e770605240e3a117f57
Rinde van Lon <rindevanlon@gmail.com>
2016-11-21 11:19:52 +0100

    force update of deps

--- mas-tuning.sh
+++ mas-tuning.sh
@@ -1,5 +1,5 @@
 export MAVEN_OPTS=-server
-nohup mvn clean compile exec:java -Dexec.mainClass="com.github.rinde.jaamas16.PerformExperiment" -Dexec.args="-exp vanlon15 \
+nohup mvn clean compile -U exec:java -Dexec.mainClass="com.github.rinde.jaamas16.PerformExperiment" -Dexec.args="-exp vanlon15 \
 --show-gui false \
 --include c32,..,c40 -r 1 \
 --scenarios.exclude p0 \
